c553fab28083c814a9708153b4f3e0968d923d62
Rami <ramimourani36@gmail.com>
2020-06-19 05:18:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Menu from './components/MenuComponent';
-
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Main from './components/MainComponent';
+// import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-		<Menu />
+        <Main/>
       </div>
     );
   }
